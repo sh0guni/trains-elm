d200a38f3778181ba5db69186267e3eccb3e9fd3
Jesse Korpela <jesse.korpela@reaktor.co.jp>
2016-02-27 15:37:54 +0900

    Fetch train schedules from OpenData api

--- Main.elm
+++ Main.elm
@@ -1,5 +1,22 @@
+import Effects exposing (Never)
 import TrainApp exposing (init, update, view)
-import StartApp.Simple exposing (start)
+import StartApp
+import Task
+
+--app =
+--  start { model = init, view = view, update = update, inputs = [] }
+
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
 
 main =
-  start { model = init, view = view, update = update }
+  app.html
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
--- TrainApp.elm
+++ TrainApp.elm
@@ -1,25 +1,35 @@
 module TrainApp where
 
+import Effects exposing (Effects, Never)
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, on, targetValue)
+import Http
 import Json.Decode exposing (..)
+import Task
+import String exposing (toInt)
 
-type alias Model = Train
+type alias Model = 
+  { train: Train
+  , trainNumber: Int
+  , departureDate: String
+  }
 
 type alias Train =
   { trainNumber: Int,
   departureDate: String
-  --,
-  --operatorUICCode: Int,
-  --operatorShortCode: String,
-  --trainType: String,
-  --trainCategory: String,
-  --runningCurrently: Bool,
-  --cancelled: Bool,
-  --version: Int
+  --timeTableRows: List TimeTableRow
   }
 
-exampleTrainJsonString = "{  \"trainNumber\": 1,  \"departureDate\": \"2015-03-01\",  \"operatorUICCode\": 10,  \"operatorShortCode\": \"vr\",  \"trainType\": \"IC\",  \"trainCategory\": \"Long-distance\",  \"runningCurrently\": false,  \"cancelled\": false,  \"version\": 3467158366,  \"timeTableRows\": [    {      \"stationShortCode\": \"HKI\",      \"stationUICCode\": 1,      \"countryCode\": \"FI\",      \"type\": \"DEPARTURE\",      \"trainStopping\": true,      \"commercialStop\": true,      \"commercialTrack\": \"6\",      \"cancelled\": false,      \"scheduledTime\": \"2015-03-01T05:12:00.000Z\"    },    {      \"stationShortCode\": \"PSL\",      \"stationUICCode\": 10,      \"countryCode\": \"FI\",      \"type\": \"ARRIVAL\",      \"trainStopping\": true,      \"commercialStop\": true,      \"commercialTrack\": \"3\",      \"cancelled\": false,      \"scheduledTime\": \"2015-03-01T05:17:00.000Z\"    }]}"
+--type alias TimeTableRow =
+--  {
+--      stationShortCode: String,
+--      type: String,
+--      scheduledTime: String
+--  }
 
+exampleTrainJsonString : String
+exampleTrainJsonString = "{  \"trainNumber\": 1,  \"departureDate\": \"2015-03-01\",  \"operatorUICCode\": 10,  \"operatorShortCode\": \"vr\",  \"trainType\": \"IC\",  \"trainCategory\": \"Long-distance\",  \"runningCurrently\": false,  \"cancelled\": false,  \"version\": 3467158366,  \"timeTableRows\": [    {      \"stationShortCode\": \"HKI\",      \"stationUICCode\": 1,      \"countryCode\": \"FI\",      \"type\": \"DEPARTURE\",      \"trainStopping\": true,      \"commercialStop\": true,      \"commercialTrack\": \"6\",      \"cancelled\": false,      \"scheduledTime\": \"2015-03-01T05:12:00.000Z\"    },    {      \"stationShortCode\": \"PSL\",      \"stationUICCode\": 10,      \"countryCode\": \"FI\",      \"type\": \"ARRIVAL\",      \"trainStopping\": true,      \"commercialStop\": true,      \"commercialTrack\": \"3\",      \"cancelled\": false,      \"scheduledTime\": \"2015-03-01T05:17:00.000Z\"    }]}"
 
 exampleTrain : String -> Train
 exampleTrain trainJsonString =
@@ -36,27 +46,95 @@ exampleTrain2 : Train
 exampleTrain2 =
   { trainNumber= 0,
   departureDate = "xx"
-  --operatorUICCode = 10,
-  --operatorShortCode = "vr",
-  --trainType = "IC",
-  --trainCategory = "Long-distance",
-  --runningCurrently = False,
-  --cancelled = False,
-  --version = 3467158366
   }
 
-init : Model
-init = exampleTrain exampleTrainJsonString
+init : (Model, Effects Action)
+init = 
+  ( { train = exampleTrain exampleTrainJsonString
+    , trainNumber = 1
+    , departureDate = "2015-03-01"
+    }
+  , Effects.none
+  )
 
 type Action
-  = Search
+  = TrainNumber Int
+  | DepartureDate String
+  | Search
+  | NewTrain (Maybe Train)
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    TrainNumber trainNumber ->
+      ( { model | trainNumber = trainNumber }
+      , Effects.none
+      )
+
+    DepartureDate departureDate ->
+      ( { model | departureDate = departureDate }
+      , Effects.none
+      )
+
+    Search ->
+      ( model
+      , getTrain model
+      )
+
+    NewTrain maybeTrain ->
+      let newTrain = Maybe.withDefault exampleTrain2 maybeTrain
+      in
+        ( { model | train = newTrain }
+        , Effects.none
+        )
+
+getTrain : Model -> Effects Action
+getTrain model =
+  Http.get trainDecoder (getOpenDataURL model.trainNumber model.departureDate)
+  |> Task.toMaybe
+  |> Task.map NewTrain
+  |> Effects.task
+
+getOpenDataURL : Int -> String -> String
+getOpenDataURL trainNumber departureDate =
+  Http.url ("http://rata.digitraffic.fi/api/v1/schedules/" ++ (toString trainNumber))
+    [ ("departure_date", departureDate) ]
 
 view : Signal.Address Action -> Model -> Html
 view address model =
   div []
-    [ div [] [ text ("TrainNumber: " ++ toString model.trainNumber)]
-    , div [] [ text ("Departure date: " ++ toString model.departureDate)]
+  [ searchBox address model
+  , trainInfo model.train
+  ]
+
+trainInfo : Train -> Html
+trainInfo train =
+  div []
+    [ div [] [ text ("TrainNumber: " ++ toString train.trainNumber) ]
+    , div [] [ text ("Departure date: " ++ toString train.departureDate) ]
     ]
 
-update : Action -> Model -> Model
-update action model = exampleTrain exampleTrainJsonString
+searchBox : Signal.Address Action -> Model -> Html
+searchBox address model =
+  div []
+    [ div []
+      [ text "Train number: "
+        , input 
+            [ placeholder (toString model.trainNumber)
+            , on "input" targetValue 
+              (\string -> Signal.message address 
+                  (TrainNumber (Result.withDefault 0 (toInt string))))
+            ] []
+        ]
+    , div []
+      [ text "Departure date: "
+        , input
+          [ placeholder (toString model.departureDate)
+          , on "input" targetValue
+          (\string -> Signal.message address
+              (DepartureDate string))
+          ] []
+        ]
+    , div []
+        [ button [ onClick address Search ] [ text "Search" ]]
+      ]
